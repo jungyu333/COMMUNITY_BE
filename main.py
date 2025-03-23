745d6439441d1064730841f30a2fe0036f635afa
jungyu333 <jungyu.kim@partridgesystems.com>
2025-03-23 13:25:56 +0900

    feat : Apply click to main.py

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
+import click
 import uvicorn
 
+from core.config import get_config
 
-def main():
+
+@click.command()
+@click.option(
+    "--env",
+    type=click.Choice(["local", "dev", "prod"], case_sensitive=False),
+    default="local",
+)
+@click.option(
+    "--debug",
+    type=click.BOOL,
+    is_flag=True,
+    default=False,
+)
+def main(env: str, debug: bool):
+    config = get_config(env)
+
+    config.DEBUG = debug or config.DEBUG
+
     uvicorn.run(
         app="app.server:app",
         host="localhost",
         port=8000,
+        reload=True if config.ENV != "production" else False,
         workers=1,
     )
 
