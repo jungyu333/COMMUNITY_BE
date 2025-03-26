fed9b9b3af3274d597280f75752341bafbf4298a
jungyu333 <jungyu3826@gmail.com>
2025-03-26 17:15:37 +0900

    feat : Add transactional.py

--- core/db/__init__.py
+++ core/db/__init__.py
@@ -1,7 +1,9 @@
 from .session import Base, session, session_factory
+from .transactional import Transactional
 
 _all_ = [
     "Base",
     "session",
     "session_factory",
+    "Transactional",
 ]
